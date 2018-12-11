ea0e849e50d70b043b66c0ba545f90900dd409a4
Renan Moraes <renan.a.moraes12@gmail.com>
2018-12-11 18:49:26 -0500

    Clean and streamline doc strings of bikshare.py file

--- bikeshare.py
+++ bikeshare.py
@@ -8,12 +8,12 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 def get_filters():
     """
-    Asks user to specify a city, month, and day to analyze.
+    Asks user to specify a city, month, and day.
 
     Returns:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        (str) city - name of the city
+        (str) month - name of the month to filter by, or "all"
+        (str) day - name of the day of week to filter by, or "all"
     """
     print('Hello! Let\'s explore some US bikeshare data!\n')
 
@@ -94,9 +94,9 @@ def load_data(city, month, day):
     Loads data for the specified city and filters by month and day if applicable.
 
     Args:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        (str) city - name of the city
+        (str) month - name of the month to filter by, or "all"
+        (str) day - name of the day of week to filter by, or "all"
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
